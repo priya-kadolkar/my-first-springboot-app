44bc5cf30b31409ab013e1622c4c6ac6d14ae162
priya-kadolkar <priyakadolkar95@gmail.com>
2022-04-25 17:48:18 +0530

    Added two numbers

---
+++ src/main/java/com/dlithe/bankingapp/practice/Test.java
@@ -0,0 +1,15 @@
+/**
+ * This class is copyright property of Dlithe organization. It is open to all and intended to use for.
+ */
+package com.dlithe.bankingapp.practice;
+
+/**
+ * This class is used to show how to commit and push the code to git
+ */
+public class Test {
+    public void addNumbers(){
+        int a=10;
+        int b=20;
+        System.out.println(a+b);
+    }
+}
