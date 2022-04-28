8fa7ef16e4dd6a152ce56667ba3f82c350dd4276
priya-kadolkar <priyakadolkar95@gmail.com>
2022-04-28 23:49:27 +0530

    Added Enity class and Repository interface, DTO class with implementation to post data and get data

--- src/main/java/com/dlithe/bankingapp/service/TestService.java
+++ src/main/java/com/dlithe/bankingapp/service/TestService.java
@@ -1,6 +1,7 @@
 package com.dlithe.bankingapp.service;
 
 import com.dlithe.bankingapp.dto.CustomerDetailsRequest;
+import com.dlithe.bankingapp.dto.CustomerDetailsResponse;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,7 +9,11 @@ public interface TestService {
 
     String fetchBankDetails(String bankName);
 
+    //Get type is used
     String loginDetails(String username, String password);
 
+    //posting type is used for this method
     String registerCustomer(CustomerDetailsRequest customerDetailsRequest);
+
+    CustomerDetailsResponse fetchCustomerDetails(int customerId);
 }
